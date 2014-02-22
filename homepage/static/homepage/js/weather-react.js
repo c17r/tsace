faa84c7d74347c5e237f27c4a116f8578afd436c
Christian Sauer <sauerc@gmail.com>
2014-02-22 16:54:55 -0500

    Make temperature information easier to understand

--- homepage/static/homepage/js/weather-react.js
+++ homepage/static/homepage/js/weather-react.js
@@ -6,7 +6,7 @@ var SearchResults = React.createClass({
            <table id="search-result">
                <tr>
                    <td className="name" colSpan="3">{this.props.name}</td>
-                   <td className="high">{this.props.temp.high}</td>
+                   <td className="high">H: {this.props.temp.high}&deg;</td>
                    <td className="add" rowSpan="2">
                        <form className="add-city" action="" method="POST">
                            <input name="key" type="hidden" value={this.props.key} />
@@ -16,9 +16,9 @@ var SearchResults = React.createClass({
                </tr>
                <tr>
                    <td className="time"><span data-tz-offset={this.props.tz_offset} /></td>
-                   <td className="current">{this.props.temp.current}</td>
+                   <td className="current">{this.props.temp.current}&deg;</td>
                    <td className="icon"><img src={"/static/homepage/img/" + this.props.temp.icon + ".png"} /></td>
-                   <td className="low">{this.props.temp.low}</td>
+                   <td className="low">L: {this.props.temp.low}&deg;</td>
                </tr>
            </table>
            );
